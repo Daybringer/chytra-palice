5800e38612a48b4c548b743bc3ef648b907a6872
Daybringer <vanata.michal@gmail.com>
2021-04-07 22:34:26 +0200

    build sh

--- build.sh
+++ build.sh
@@ -4,6 +4,19 @@ echo "Installing global dependencies"
 yarn global add @vue/cli
 npm global add serve
 
+git submodule foreach git pull
+
+cd frontend
+
+yarn
+
+cd ..
+
+cd server
+
+yarn
+
+cd ..
 
 
 
